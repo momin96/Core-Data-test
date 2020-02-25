fb7eca75a1f9d15fc7c470be403521b2e8e274e9
Nasir Ahmed Momin <nasir_momin@apple.com>
2020-02-25 13:24:06 +0530

    Initial Commit

---
+++ Core data text/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core data text
+//
+//  Created by Nasir Ahmed Momin on 25/02/20.
+//  Copyright © 2020 Nasir Ahmed Momin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
